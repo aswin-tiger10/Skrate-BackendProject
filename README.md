b0d4688810ad58bc96b5cc5972839507cb6ecda5
Aswin <saswin0502@gmail.com>
2022-08-08 23:18:22 +0530

    Revert "initial"
    This reverts commit ffaea70eeb4e463dc269b3042bd0ff7778ac0373.

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # Skrate-BackendProject
-
-To run the project :
-
-    1. Run 'npm install' to install all packages
-    2. Run 'npm start' to run the project
--- models/tokenSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const tokenSchema = new mongoose.Schema({
-  token: { type: String },
-  userName: { type: String },
-  role: { type: String },
-});
-
-module.exports = mongoose.model("token", tokenSchema);
